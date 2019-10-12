dfd7c7a8df306b31bfa34d1850e00ead6194404f
vladar21 <vladar21@gmail.com>
2019-10-13 01:18:29 +0300

    add form creation

--- ADOMET_DZ006_Rastvorov/Form1.Designer.cs
+++ ADOMET_DZ006_Rastvorov/Form1.Designer.cs
@@ -28,13 +28,126 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.BooksPage = new System.Windows.Forms.TabPage();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.AuthorsPage = new System.Windows.Forms.TabPage();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.PublishersPage = new System.Windows.Forms.TabPage();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.tabControl1.SuspendLayout();
+            this.BooksPage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.AuthorsPage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.PublishersPage.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl1.Controls.Add(this.BooksPage);
+            this.tabControl1.Controls.Add(this.AuthorsPage);
+            this.tabControl1.Controls.Add(this.PublishersPage);
+            this.tabControl1.Location = new System.Drawing.Point(12, 67);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(575, 354);
+            this.tabControl1.TabIndex = 0;
+            // 
+            // BooksPage
+            // 
+            this.BooksPage.Controls.Add(this.dataGridView1);
+            this.BooksPage.Location = new System.Drawing.Point(4, 22);
+            this.BooksPage.Name = "BooksPage";
+            this.BooksPage.Padding = new System.Windows.Forms.Padding(3);
+            this.BooksPage.Size = new System.Drawing.Size(567, 328);
+            this.BooksPage.TabIndex = 0;
+            this.BooksPage.Text = "Books";
+            this.BooksPage.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(561, 322);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_UserAddedRow);
+            // 
+            // AuthorsPage
+            // 
+            this.AuthorsPage.Controls.Add(this.dataGridView2);
+            this.AuthorsPage.Location = new System.Drawing.Point(4, 22);
+            this.AuthorsPage.Name = "AuthorsPage";
+            this.AuthorsPage.Padding = new System.Windows.Forms.Padding(3);
+            this.AuthorsPage.Size = new System.Drawing.Size(567, 328);
+            this.AuthorsPage.TabIndex = 1;
+            this.AuthorsPage.Text = "Authors";
+            this.AuthorsPage.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(561, 322);
+            this.dataGridView2.TabIndex = 0;
+            this.dataGridView2.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_UserAddedRow);
+            // 
+            // PublishersPage
+            // 
+            this.PublishersPage.Controls.Add(this.dataGridView3);
+            this.PublishersPage.Location = new System.Drawing.Point(4, 22);
+            this.PublishersPage.Name = "PublishersPage";
+            this.PublishersPage.Size = new System.Drawing.Size(567, 328);
+            this.PublishersPage.TabIndex = 2;
+            this.PublishersPage.Text = "Publishers";
+            this.PublishersPage.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView3.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(567, 328);
+            this.dataGridView3.TabIndex = 0;
+            this.dataGridView3.UserAddedRow += new System.Windows.Forms.DataGridViewRowEventHandler(this.dataGridView1_UserAddedRow);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(595, 450);
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.tabControl1.ResumeLayout(false);
+            this.BooksPage.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.AuthorsPage.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.PublishersPage.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage BooksPage;
+        private System.Windows.Forms.TabPage AuthorsPage;
+        private System.Windows.Forms.TabPage PublishersPage;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.DataGridView dataGridView3;
     }
 }
 
--- ADOMET_DZ006_Rastvorov/Form1.cs
+++ ADOMET_DZ006_Rastvorov/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,137 @@ namespace ADOMET_DZ006_Rastvorov
         public Form1()
         {
             InitializeComponent();
+            LoadData();
+        }
+
+        public void LoadData()
+        {
+            using (LibraryEntities db = new LibraryEntities())
+            {
+                // заполняем таблицу вкладки Authors
+                DataTable dtBooks = new DataTable();
+                dtBooks.Columns.Add("Id");
+                dtBooks.Columns.Add("Title");
+                dtBooks.Columns.Add("IdAuthor");
+                dtBooks.Columns.Add("Pages");
+                dtBooks.Columns.Add("Price");
+                dtBooks.Columns.Add("IdPublisher");
+
+                var bks = db.Book.ToList();
+                foreach (var b in bks)
+                {
+                    DataRow dr = dtBooks.NewRow();
+                    dr["Id"] = b.Id;
+                    dr["Title"] = b.Title;
+                    dr["IdAuthor"] = b.IdAuthor;
+                    dr["Pages"] = b.Pages;
+                    dr["Price"] = b.Price;
+                    dr["IdPublisher"] = b.IdPublisher;
+                    dtBooks.Rows.Add(dr);
+                }
+                dataGridView1.DataSource = null;
+                dataGridView1.DataSource = dtBooks;
+
+                // заполняем таблицу вкладки Authors
+                DataTable dtAuthors = new DataTable();
+                dtAuthors.Columns.Add("Id");
+                dtAuthors.Columns.Add("FirstName");
+                dtAuthors.Columns.Add("LastName");
+
+                var au = db.Author.ToList();
+                foreach (var a in au)
+                {
+                    DataRow dr = dtAuthors.NewRow();
+                    dr["Id"] = a.Id;
+                    dr["FirstName"] = a.FirstName;
+                    dr["LastName"] = a.LastName;
+                    dtAuthors.Rows.Add(dr);
+                }
+                dataGridView2.DataSource = null;
+                dataGridView2.DataSource = dtAuthors;
+
+                // заполняем таблицу вкладки Publisher
+                DataTable dtPublishers = new DataTable();
+                dtPublishers.Columns.Add("Id");
+                dtPublishers.Columns.Add("PublisherName");
+                dtPublishers.Columns.Add("Address");
+
+                var pb = db.Publisher.ToList();
+                foreach (var p in pb)
+                {
+                    DataRow dr = dtPublishers.NewRow();
+                    dr["Id"] = p.Id;
+                    dr["PublisherName"] = p.PublisherName;
+                    dr["Address"] = p.Address;
+                    dtPublishers.Rows.Add(dr);
+                }
+                dataGridView3.DataSource = null;
+                dataGridView3.DataSource = dtPublishers;
+            }
+
+        }
+
+        private void dataGridView1_UserAddedRow(object sender, DataGridViewRowEventArgs e)
+        {         
+
+            Form addForm = new Form();
+            addForm.Size = new Size(400, 550);
+            Label addLabel = new Label();
+            addLabel.Name = "Title";
+            addLabel.Location = new Point(22, 12);
+            addLabel.AutoSize = true;
+            addLabel.Font = new Font("Arial", 21, FontStyle.Bold);
+            switch ((sender as DataGridView).Parent.Name)
+            {
+                case "BooksPage":
+                    addLabel.Text = "ADD NEW BOOK:";                    
+                    break;
+                case "AuthorsPage":
+                    addLabel.Text = "ADD NEW AUTHOR:";
+                    break;
+                case "PublishersPage":
+                    addLabel.Text = "ADD NEW PUBLISHER:";
+                    break;
+            }
+
+            DataTable addTable = new DataTable();
+            DataGridView addDGr = new DataGridView();
+            addDGr = sender as DataGridView;
+            int shag = 22;
+            foreach (DataGridViewColumn column in addDGr.Columns)
+            {
+                Label label = new Label();
+                TextBox textbox = new TextBox();                
+                shag += 60;
+                label.Location = new Point(22, shag);
+                label.AutoSize = true;
+                label.Text = column.HeaderText;
+                label.Font = new Font("Arial", 18, FontStyle.Bold);
+                addForm.Controls.Add(label);
+                textbox.Location = new Point(22, shag + 30);
+                textbox.Size = new Size(300, 50);
+                textbox.Font = new Font("Arial", 16, FontStyle.Bold);
+                textbox.Name = column.HeaderText;
+                addForm.Controls.Add(textbox);
+            }
+
+            Button addButton = new Button();
+            addButton.Text = "Add";
+            addButton.AutoSize = true;
+            addButton.Font = new Font("Arial", 14, FontStyle.Bold);
+            addButton.Location = new Point(22, 50);            
+            addForm.Controls.Add(addButton);
+            addButton.Click += AddButton_Click;
+
+            //addForm.Controls.Add(addDGr);
+            addForm.Controls.Add(addLabel);            
+            addForm.Show();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show();
+            
         }
     }
 }
